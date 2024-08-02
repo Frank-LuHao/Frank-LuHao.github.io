b6c85bff494f71a1e43e2470a9b694fcac727b66
Frank-LuHao <2533768148@qq.com>
2024-08-02 17:09:40 +0800

    Site updated: 2024-08-02 17:09:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/31/hello-world/"];function toRandomPost(){
+var posts=["2024/07/31/hello-world/","2024/08/02/The-Zen-of-Python/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
